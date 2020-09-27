9b82cc799c4430651c30ccc028f5ff16822a59a5
Roman Levinzon <levinzon.roman@gmail.com>
2020-09-27 15:28:41 +0200

    Init commit

---
+++ app/src/main/java/cz/levinzonr/weatherapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package cz.levinzonr.weatherapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.Text
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.setContent
+import androidx.ui.tooling.preview.Preview
+import cz.levinzonr.weatherapp.ui.WeatherAppTheme
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            WeatherAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WeatherAppTheme {
+        Greeting("Android")
+    }
+}
